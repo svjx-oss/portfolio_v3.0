47a19f579d6a12876d84225e2aeacd4b3ec4a1c3
Sahil J <dev.sahil.jaganmohan@gmail.com>
2026-09-07 11:06:54 -0700

    refactor: use lighter variable Inter prose
    Load Inter variable fonts and use a lighter shared body weight.
    
    ref: ME-6

--- portfolio/routes/_app.tsx
+++ portfolio/routes/_app.tsx
@@ -11,7 +11,7 @@ export default define.page(function App(ctx) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link
           rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Inter:ital,wght@0,400;0,500;0,600;0,700;1,400&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
         />
         <Seo site={site} pathname={ctx.url.pathname} />
       </head>
