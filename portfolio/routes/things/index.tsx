0af09d1151a92d34c51463fff975c5b4b9a89dda
Sahil J <dev.sahil.jaganmohan@gmail.com>
2026-09-12 16:55:36 -0700

    feat: add Things content foundation
    Load and validate published Things entries with production-safe development fixtures.
    
    ref: ME-25

--- portfolio/routes/things/index.tsx
+++ portfolio/routes/things/index.tsx
@@ -1,6 +1,13 @@
 import ThingsPage from "@/components/ThingsPage.tsx";
+import { loadThingsIndex } from "@/lib/loadContent.ts";
 import { define } from "@/utils.ts";
 
-export default define.page(function Things() {
-  return <ThingsPage />;
+export const handler = define.handlers({
+  async GET() {
+    return { data: { things: await loadThingsIndex() } };
+  },
+});
+
+export default define.page<typeof handler>(function Things({ data }) {
+  return <ThingsPage {...data.things} />;
 });
