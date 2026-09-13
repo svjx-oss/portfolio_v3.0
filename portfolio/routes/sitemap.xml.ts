9b89a0ca5f29b73ad3c0f14d8f735b8504aee839
Sahil J <dev.sahil.jaganmohan@gmail.com>
2026-09-13 13:10:17 -0700

    feat: add site discovery endpoints
    Publish a site-wide sitemap and an RSS feed for public internal Things posts.
    
    ref: ME-13

---
+++ portfolio/routes/sitemap.xml.ts
@@ -0,0 +1,27 @@
+import { loadPublishedThingsPosts, loadSite } from "@/lib/loadContent.ts";
+import { escapeXml } from "@/lib/xml.ts";
+
+export const handler = {
+  async GET() {
+    const [site, posts] = await Promise.all([
+      loadSite(),
+      loadPublishedThingsPosts(),
+    ]);
+    const urls = [
+      "/",
+      "/about",
+      "/experience",
+      "/things?type=all",
+      ...posts.map((post) => `/things/${post.slug}`),
+    ];
+    const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${
+      urls.map((path) => `
+  <url><loc>${escapeXml(`${site.url}${path}`)}</loc></url>`).join("")
+    }
+</urlset>`;
+    return new Response(xml, {
+      headers: { "content-type": "application/xml; charset=utf-8" },
+    });
+  },
+};
